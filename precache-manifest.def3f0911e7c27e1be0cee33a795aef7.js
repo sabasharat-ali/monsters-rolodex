071747cbb28cb332e00b3ac243d85c3ba61b2f71
sabasharat-ali <sabasharat.ali@gmail.com>
2020-03-19 19:25:27 +0500

    Updates

---
+++ precache-manifest.def3f0911e7c27e1be0cee33a795aef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6fb175366f2b894c70b9c9ee654226",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "23a848c5bc10b9bb1158",
+    "url": "/monsters-rolodex/static/css/main.f69c7688.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a848c5bc10b9bb1158",
+    "url": "/monsters-rolodex/static/js/main.da70f04f.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
